e454df4ee487fe5c0dc99fa4beb145ba0c282544
satyajitsinha1919 <90083670+satyajitsinha1919@users.noreply.github.com>
2021-09-23 13:28:36 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import './App.css';
-import {
-  BrowserRouter as Router
-} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Loader from "./components/Loader";
+// import Home from "./components/Home";
+import Services from "./components/Services";
+// import About from "./components/About";
 function App() {
   return (
     <>
-    <Loader/>
-    <div className="hhh">Welcome to SatyaJit WorkSpace</div>
-    <Router>
-      <Navbar brand = "SatyaJit"/>
-    </Router>
+      <Loader />
+      <div className="hhh">Welcome to SatyaJit WorkSpace</div>
+      <Router>
+        <Navbar brand="SatyaJit" />
+        <Services/>
+        {/* <Switch>
+        <Route exact path = "/">
+          <Home/>
+        </Route>
+        <Route exact path = "/services">
+          <Services/>
+        </Route>
+        <Route exact path = "/about">
+          <About/>
+        </Route>
+      </Switch> */}
+      </Router>
     </>
   );
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,37 @@
 import React from "react";
 import "./Services.css";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 export default function Services() {
   return (
     <div className="services">
-      <Helmet>
+      {/* <Helmet>
         <title>SatyaJit | Services</title>
-      </Helmet>
+      </Helmet> */}
       <h1>Services</h1>
+      <div className="container">
+        <div className="service-box">
+          <ul className="service-menu">
+            <li className="service-item">
+              <a href="https://docs.google.com/spreadsheets/d/1_Tfy236aj3aip8qfJ0pbfNyzaPsKikl81uPUj_RA6Mw" className="service-link" target="_blank" rel="noreferrer"><i className="fas fa-file-alt"></i> Work Sheet</a>
+            </li>
+            <li className="service-item">
+              <a href="/" className="service-link"><i className="fas fa-file-alt"></i> Priority Sheet</a>
+            </li>
+            <li className="service-item">
+              <a href="/" className="service-link"><i className="fas fa-file-alt"></i> Count Sheet</a>
+            </li>
+            <li className="service-item">
+              <a href="/" className="service-link"><i className="fas fa-file-alt"></i> Audit Sheet</a>
+            </li>
+            <li className="service-item">
+              <a href="/" className="service-link"><i className="fas fa-file-alt"></i> Hourly Audit Sheet</a>
+            </li>
+            <li className="service-item">
+              <a href="/" className="service-link"><i className="fas fa-file-alt"></i> Call Tracker Sheet</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
