12111bd207cf5dd31664a5e0a3c01298c68e2807
satyajitsinha1919 <90083670+satyajitsinha1919@users.noreply.github.com>
2021-09-23 17:05:36 +0530

    Update

--- src/components/Services.js
+++ src/components/Services.js
@@ -20,16 +20,16 @@ export default function Services() {
               <a href="https://docs.google.com/spreadsheets/d/1y5NXKMgCgbNuUTYeMnbk2KGJ_lSfvCp1ZGNh2NEvpnU" className="service-link" target="_blank" rel="noreferrer"><i className="fas fa-file-alt"></i> Priority Sheet</a>
             </li>
             <li className="service-item">
-              <a href="https://docs.google.com/spreadsheets/d/1UQ-Sis33M6iDJx4vJwnvUwyBJLjHSczmyrdmhfWXF4g" className="service-link"><i className="fas fa-file-alt" target="_blank" rel="noreferrer"></i> Count Sheet</a>
+              <a href="https://docs.google.com/spreadsheets/d/1UQ-Sis33M6iDJx4vJwnvUwyBJLjHSczmyrdmhfWXF4g" className="service-link" target="_blank" rel="noreferrer"><i className="fas fa-file-alt"></i> Count Sheet</a>
             </li>
             <li className="service-item">
-              <a href="/" className="service-link"><i className="fas fa-file-alt" target="_blank" rel="noreferrer"></i> Audit Sheet</a>
+              <a href="https://docs.google.com/spreadsheets/d/1uCUTpCGl0Jd7koI1fok8ft4UT_d1B6Ina_-tP2aD1kU" className="service-link" target="_blank" rel="noreferrer"><i className="fas fa-file-alt"></i> Audit Sheet</a>
             </li>
             <li className="service-item">
-              <a href="/" className="service-link"><i className="fas fa-file-alt" target="_blank" rel="noreferrer"></i> Hourly Audit Sheet</a>
+              <a href="https://docs.google.com/spreadsheets/d/1idFo5miNhCmsjIusixpzXZi3YXe7DIWAjHwRzl-jYgY" className="service-link" target="_blank" rel="noreferrer"><i className="fas fa-file-alt"></i> Hourly Audit Sheet</a>
             </li>
             <li className="service-item">
-              <a href="/" className="service-link"><i className="fas fa-file-alt" target="_blank" rel="noreferrer"></i> Call Tracker Sheet</a>
+              <a href="https://docs.google.com/spreadsheets/d/17YnMFcqDIgw6c4P7VDiYVRbFa1eygn6dbbE2wcqSdxk" className="service-link" target="_blank" rel="noreferrer"><i className="fas fa-file-alt"></i> Call Tracker Sheet</a>
             </li>
           </ul>
         </div>
